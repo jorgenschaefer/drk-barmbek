147f5e32778b6122b29bc7bbc018e2c9256591ed
Jorgen Schaefer <Jorgen.Schaefer@mindmatters.de>
2020-06-11 10:52:44 +0200

    Überarbeitung der Link-Page

--- src/LinkPage.js
+++ src/LinkPage.js
@@ -11,20 +11,24 @@ export default function LinkPage() {
         <li><ExternalLink href="http://hhn.hiorg-server.de/">HiOrg-Server</ExternalLink></li>
         <li><ExternalLink href="https://plato.lv-hamburg.drk.de/">PLATO (Ausbildungen am Landesverband)</ExternalLink></li>
       </ul>
-      <Header>Neue Helfende</Header>
+      <Header>Aus- und Fortbildungen</Header>
       <ul>
         <li><ExternalLink href="https://www.erste-hilfe-hamburg.org/">Erste-Hilfe-Kurs Anmeldungen</ExternalLink></li>
+        <li><ExternalLink href="https://www.drk-lerncampus.de/">DRK Lerncampus</ExternalLink></li>
       </ul>
-      <Header>Erfahrene Helfende</Header>
+      <Header>Sanitätsdienste</Header>
+      <ul>
+        <li><ExternalLink href="http://domplan.drk-altona-mitte.de/">DOM-Anmeldungen</ExternalLink></li>
+      </ul>
+      <Header>Orga-Arbeit Bereitschaft Barmbek</Header>
       <ul>
         <li><ExternalLink href="https://trello.com/drkbarmbekuhlenhorst/home">Trello für Orga-Arbeit</ExternalLink></li>
-        <li><ExternalLink href="https://www.drk-intern.de/">Interner Bereich des DRK</ExternalLink></li>
-        <li><ExternalLink href="http://domplan.drk-altona-mitte.de/">DOM-Anmeldungen</ExternalLink></li>
       </ul>
       <Header>Verschiedenes</Header>
       <ul>
         <li><ExternalLink href="https://www.drk-hamburg-nordost.de/">Homepage des KV</ExternalLink></li>
         <li><ExternalLink href="https://www.facebook.com/drkhamburgnordost/">Facebook-Page des KV</ExternalLink></li>
+        <li><ExternalLink href="https://www.drk-intern.de/">Interner Bereich des DRK</ExternalLink></li>
         <li><ExternalLink href="https://www.rotkreuzshop.de/">Rotkreuz-Shop</ExternalLink></li>
         <li><ExternalLink href="https://www.drkservice.de/rahmenvertraege/">Rahmenverträge für Rotkreuzmitarbeiter</ExternalLink></li>
         <li><ExternalLink href="https://www.wecanhelp.de/moin-menschlichkeit">Einkaufen mit kostenfreier Spende an den KV</ExternalLink></li>
