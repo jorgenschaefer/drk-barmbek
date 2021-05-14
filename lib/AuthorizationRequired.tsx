b4e459a28b05527a1184646237182fa95baac644
Jorgen Schaefer <Jorgen.Schaefer@mindmatters.de>
2021-05-14 18:17:08 +0200

    wip

--- lib/AuthorizationRequired.tsx
+++ lib/AuthorizationRequired.tsx
@@ -1,10 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import { colors } from "@barmbek/DRKStyle";
 
 const AuthorizationRequired = ({ onAuthorize }: { onAuthorize: () => void }) => (
   <>
-    <p>Diese Seiten sind nur für Mitglieder der Bereitschaft Barmbek bestimmt. Bist du Mitglied der Bereitschaft Barmbek?</p>
-    <button type="button" onClick={onAuthorize}>Ja, ich bin Mitglied der Bereitschaft Barmbek</button>
+    <Question>Diese Seiten sind nur für Mitglieder der Bereitschaft Barmbek bestimmt. Bist du Mitglied der Bereitschaft Barmbek?</Question>
+    <AuthButton onClick={onAuthorize}>Ja, ich bin Mitglied der Bereitschaft Barmbek</AuthButton>
   </>
 );
+
+const Question = styled.div`
+  max-width: 60ch;
+  margin-bottom: 2ch;
+`;
+
+const AuthButton = styled.div`
+  border: 1px solid transparent;
+  border-radius: 1ch;
+  background: ${colors.midblue};
+  color: ${colors.lightgray};
+  font-weight: bold;
+  max-width: 60ch;
+  padding: 0.5rem;
+  cursor: pointer;
+  text-align: center;
+`
+
+
 
 export default AuthorizationRequired;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+
+const data = [
+  {
+    id: "a1",
+    title: "Aufgabe 1",
+    text: "lorem ipsum dolor sit amet",
+  },
+  {
+    id: "a2",
+    title: "Aufgabe 2",
+    text: "lorem ipsum dolor sit amet",
+  },
+  {
+    id: "a3",
+    title: "Aufgabe 3",
+    text: "lorem ipsum dolor sit amet",
+  },
+  {
+    id: "a4",
+    title: "Aufgabe 4",
+    text: "lorem ipsum dolor sit amet",
+  },
+  {
+    id: "a5",
+    title: "Aufgabe 5",
+    text: "lorem ipsum dolor sit amet",
+  },
+]
+
+
 
 export default function HomePage() {
-  return <p>Willkommen!</p>;
+  return (
+    <TaskList>
+      { data.map((elt) => <ListItem key={elt.id} title={elt.title} text={elt.text} />) }
+    </TaskList>
+  );
 }
 
+const TaskList = styled.ul``;
+
+const ListItem: React.FC<{ title: string, text: string }> = ({ title, text }) => (
+  <li><input type="checkbox" />{title}</li>
+);
